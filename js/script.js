1aee92ca8cf88f0b579e3a340e9a5697079b8d08
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-06-24 16:01:03 +0200

    Add Card Deck Array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Add Math Function
+// Set RNG
 let rndmNumb = Math.floor(Math.random() * 52);
-console.log(rndmNumb); //WORKS
-// Declare variables and set RNG
+console.log("rng " + rndmNumb); //WORKS
+// Add card deck as array
+var cardDeck = [
+    2,3,4,5,6,7,8,9,10,10,10,11,
+    2,3,4,5,6,7,8,9,10,10,10,11,
+    2,3,4,5,6,7,8,9,10,10,10,11,
+    2,3,4,5,6,7,8,9,10,10,10,11,
+];
+//console.log(cardDeck); TEST - REMOVE LATER
+// Draw a card from card deck Array
+
+
+
+
 
