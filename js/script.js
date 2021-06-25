99e0b59de0a4b4f4ad0f293e2cc2225a7843c001
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-06-25 14:57:04 +0200

    Add functional restart button in HTML to avoid to much eventlisteners.

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ let cardDeck = [
 var maxCards = cardDeck.length;
 var playerHand;
 var dealerHand;
+var UserPoints;
 // Draw a card from card deck Array
 function getRandomInt(maxCards){
     var randomCard = Math.floor(cardDeck.length * Math.random());
@@ -23,7 +24,7 @@ function startGame() {
 }
 function getHandValue(hand){
     var sum = 0;
-    for (var i = 0; i < hand.length ; i++ ) { // Once i = 2 we break out the loop
+    for (var i = 0; i < hand.length ; i++ ) { 
         sum += hand[i]
     }
     return sum;
@@ -38,12 +39,24 @@ document.getElementById("hit").onclick = function(){
     document.getElementById("dealer-hand-value").innerHTML = getHandValue(dealerHand);
     if (getHandValue(playerHand) > 21) {
         document.getElementById("player-hand-value").innerHTML = "BUST ! you drew " + getHandValue(playerHand);
-        document.getElementById("game-status").innerHTML = "PLAYER BUST"
+        document.getElementById("game-status").innerHTML = "PLAYER BUST";
     } else if (getHandValue(dealerHand) > 21) {
         document.getElementById("dealer-hand-value").innerHTML = "BUST ! dealer drew " + getHandValue(dealerHand);
         document.getElementById("game-status").innerHTML = "DEALER BUST"
     }
     
+}
+
+function win(bn){
+	UserPoints++;
+    document.getElementById("who").innerHTML = "You win!";
+    var bn = document.getElementById(bn);
+    bn.classList.remove("bn");
+    bn.classList.add("green");
+    setTimeout(() => {
+    	bn.classList.add("bn");
+        bn.classList.remove("green");
+    }, 1200);
 }
 
 /*const mode;
@@ -55,6 +68,9 @@ function draw () {
     clear();
     if (mode == 0) {
         document.getElementById("game-status").innerHTML = "PRESS ENTER TO CONTINUE";
+    }
+    if (mode == 1) {
+        
     }
 }
 
